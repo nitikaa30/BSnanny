25cb293e39193e11cd929d8365dc5ceb0bca0921
Nitika3007 <69976614+Nitika3007@users.noreply.github.com>
2023-06-20 16:24:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bsnanny/fragments/splash/Splash.kt
@@ -0,0 +1,34 @@
+package com.example.bsnanny.fragments.splash
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.bsnanny.R
+import com.example.bsnanny.databinding.FragmentSplashBinding
+
+
+class Splash : Fragment() {
+    private lateinit var binding: FragmentSplashBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentSplashBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Looper.myLooper()?.let {
+            Handler(it).postDelayed(Runnable{
+                findNavController().navigate(R.id.action_splash_to_intro)
+            },3000)
+        }
+    }
+
+}
