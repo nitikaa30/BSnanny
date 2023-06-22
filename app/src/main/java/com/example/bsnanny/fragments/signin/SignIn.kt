978a090d4855e80c9792b87e48a6895edcb999cd
arpit316 <arpit030j@gmail.com>
2023-06-22 13:03:17 +0530

    test commit

--- app/src/main/java/com/example/bsnanny/fragments/signin/SignIn.kt
+++ app/src/main/java/com/example/bsnanny/fragments/signin/SignIn.kt
@@ -12,11 +12,6 @@ import com.example.bsnanny.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SignIn.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignIn : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
