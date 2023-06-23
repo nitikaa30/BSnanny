848552ec445b95360d37b461a27f59499cba761b
arpit316 <arpit030j@gmail.com>
2023-06-23 17:03:10 +0530

    added jobcard full

--- app/src/main/java/com/example/bsnanny/fragments/dashboard/Dashboard.kt
+++ app/src/main/java/com/example/bsnanny/fragments/dashboard/Dashboard.kt
@@ -1,12 +1,16 @@
 package com.example.bsnanny.fragments.dashboard
 
+import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.app.ActivityCompat
 import androidx.navigation.fragment.findNavController
 import com.example.bsnanny.R
@@ -22,6 +26,7 @@ class Dashboard : Fragment() {
     private lateinit var binding : FragmentDashboardBinding
     private var textLatitude: Double = 0.0
     private var textLongitude: Double = 0.0
+    private lateinit var actionDrawerToggle: ActionBarDrawerToggle
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,6 +46,7 @@ class Dashboard : Fragment() {
         binding.manualAddressAdd.setOnClickListener {
             findNavController().navigate(R.id.action_dashboard_to_jobCard)
         }
+        setUpDrawer()
 
     }
     private fun getLocation() {
@@ -83,6 +89,51 @@ class Dashboard : Fragment() {
                 binding.mapView.getMapboxMap().setCamera(initialCameraOptions)
             }
         }
+    }
+    private fun setUpDrawer() {
+        actionDrawerToggle = ActionBarDrawerToggle(
+            requireActivity(),
+            binding.mainDrawer,
+            binding.dashboardToolbar,
+            R.string.open,
+            R.string.close
+        )
+
+        val headerView = binding.navView
+        val header = headerView.getHeaderView(0)
+        //val title = header.findViewById<TextView>(R.id.name_drawer)
+//        title.text = name
+//        actionDrawerToggle.apply {
+//            setHomeAsUpIndicator(R.drawable.menu)
+//        }
+
+
+
+
+        binding.mainDrawer.addDrawerListener(actionDrawerToggle)
+        actionDrawerToggle.syncState()
+
+      //  binding.navView.menu.findItem(R.id.menu_logout).setTitleColor(Color.parseColor("#FF9C1C"))
+
+        binding.navView.setNavigationItemSelectedListener {
+            it.isChecked
+            try {
+                when (it.itemId) {
+
+
+
+                    else -> {
+                        true
+                    }
+                }
+            }catch (e : Exception){
+                e.printStackTrace()
+                true
+            }
+
+
+        }
+
     }
 
 }
