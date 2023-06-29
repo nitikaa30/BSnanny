ebcaedd6e11b11dbb6983735ebcef5825e12325d
Nitika3007 <69976614+Nitika3007@users.noreply.github.com>
2023-06-29 10:26:57 +0530

    profile view model added

--- app/src/main/java/com/example/bsnanny/fragments/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/bsnanny/fragments/profile/ProfileViewModel.kt
@@ -1,9 +1,28 @@
 package com.example.bsnanny.fragments.profile
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ProfileViewModel : ViewModel() {
-    val genderSelect: MutableLiveData<String> = MutableLiveData("Boy")
-    val editMode: MutableLiveData<Boolean> = MutableLiveData(false)
+    private val _genderSelect = MutableLiveData<String>()
+    val genderSelect: LiveData<String> = _genderSelect
+
+    private val _isProfileEditable = MutableLiveData<Boolean>()
+    val isProfileEditable: LiveData<Boolean> = _isProfileEditable
+
+    private val _isProfileVisible = MutableLiveData<Boolean>()
+    val isProfileVisible: LiveData<Boolean> = _isProfileVisible
+
+    fun setGenderSelect(gender: String) {
+        _genderSelect.value = gender
+    }
+
+    fun setProfileEditable(editable: Boolean) {
+        _isProfileEditable.value = editable
+    }
+
+    fun setProfileVisible(visible: Boolean) {
+        _isProfileVisible.value = visible
+    }
 }
